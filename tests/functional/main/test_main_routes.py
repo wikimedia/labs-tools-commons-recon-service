3fff65473615b9b266be32b395509b01221846f7
eugene233 <agboreugene@gmail.com>
2021-09-08 09:51:13 +0100

    Add support for Pytest and Coverage to reconciliation repository
    * Added Pytest and configuration to project
    * Added coverage and configuration to project
    
    Bug: T290550
    Change-Id: I659f2448473320eb4dbd48eb8f8de0e96d3278ea

---
+++ tests/functional/main/test_main_routes.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Author: Eugene Egbe
+# Unit tests for the main routes in the reconciliation service
+
+import unittest
+
+from flask import session
+
+from service import app
+
+
+class TestMain(unittest.TestCase):
+    
+    # setup and teardown #
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+    # executed after each test
+    def tearDown(self):
+        pass
+    
+    # tests #
+
+    def test_home_route(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
