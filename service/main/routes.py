9168a5c96cf98dab8bff865c09294ee4560825ed
eugene233 <agboreugene@gmail.com>
2021-09-30 20:18:32 +0100

    Commons recon service endpoint mismatches the one mentioned on the landing page
    * Point recon service endpoint to correct landing page
    * Modified tox ini file with flake commonds
    
    Bug: T291461
    Change-Id: I6ca0d84e396fb2612fc83671c1de679080fe1809

--- service/main/routes.py
+++ service/main/routes.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Blueprint, url_for, redirect, render_template
+from flask import Blueprint, url_for, redirect, render_template, request
 
 
 main = Blueprint('main', __name__)
@@ -8,4 +8,6 @@ main = Blueprint('main', __name__)
 
 @main.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('main/home.html', title='Home')
+    return render_template('main/home.html',
+                           host=request.host_url + 'en/api',
+                           title='Home')
